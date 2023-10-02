c1abce5982374a327b3eff119e93b593a8afc275
Amir <ja.amir2002@gmai.com>
2023-10-02 10:30:17 +0330

    change docker-compose config

---
+++ init-scripts/init.sql
@@ -0,0 +1,8 @@
+-- init.sql
+CREATE DATABASE mydb;
+CREATE USER myuser WITH PASSWORD 'mypassword';
+ALTER ROLE myuser SET client_encoding TO 'utf8';
+ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';
+ALTER ROLE myuser SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+
