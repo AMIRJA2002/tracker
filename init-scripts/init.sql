e403357ad45a7574a66dc93085664eff9f40ecba
Amir <ja.amir2002@gmai.com>
2023-10-02 13:24:17 +0330

    fix docker bug

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,6 +1,6 @@
--- init.sql
+
 CREATE DATABASE tracker_db;
-CREATE USER tracker_user WITH PASSWORD 'tracker_pass' SUPERUSER; -- Add SUPERUSER privilege
+CREATE USER tracker_user WITH PASSWORD 'tracker_pass' SUPERUSER;
 ALTER ROLE tracker_user SET client_encoding TO 'utf8';
 ALTER ROLE tracker_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE tracker_user SET timezone TO 'UTC';
